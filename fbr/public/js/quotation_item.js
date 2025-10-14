635c21ef22839b6dd19db4d3a6162336c70a7f1a
SufyanRehman1 <sufyan@unisolerp.com>
2025-10-14 15:56:39 +0500

    Added MT And MT Rate Filed along with Functionality

---
+++ fbr/public/js/quotation_item.js
@@ -0,0 +1,20 @@
+
+frappe.ui.form.on("Quotation Item", {
+    custom_mt: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.custom_mt) {
+            row.qty = row.custom_mt * 1000;
+            frm.refresh_field("items");
+        }
+    },
+
+    custom_mt_rate: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.custom_mt_rate) {
+            row.rate = row.custom_mt_rate / 1000;
+            frm.refresh_field("items");
+        }
+    }
+});
+
+
